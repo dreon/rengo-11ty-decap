8ab5019fb0555821994ebc2fc52f05981328a712
Matthew Dreon <matthew@dreon.org>
2024-01-09 23:07:23 -0600

    Fix

--- .eleventy.js
+++ .eleventy.js
@@ -13,7 +13,10 @@ const dates = require("./js/dates.js");
 
 module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
-  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy({
+    "./assets":"./assets",
+    "./admin/config.yml": "./admin/config.yml",
+  });
 
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
